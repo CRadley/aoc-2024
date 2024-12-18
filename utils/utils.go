f65f33206131793a692ed4f133ea07d9f21d5640
Connor Radley <61577912+CRadley@users.noreply.github.com>
2024-12-18 10:51:51 +0000

    Add utils updates

--- utils/utils.go
+++ utils/utils.go
@@ -11,3 +11,10 @@ func Abs(x, y int) int {
 func IsWithinBounds(i, j int, lines [][]string) bool {
 	return (i >= 0 && i < len(lines)) && (j >= 0 && j < len(lines[0]))
 }
+func IsWithinBounds3(i, j int, lines [][]int) bool {
+	return (i >= 0 && i < len(lines)) && (j >= 0 && j < len(lines[0]))
+}
+
+func IsWithinBounds2(i, j int, lines []string) bool {
+	return (i >= 0 && i < len(lines)) && (j >= 0 && j < len(lines[0]))
+}
